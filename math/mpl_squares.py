78ce2f1f31e6e90b0878c9ca9d1896d5d8becd56
unibeam <unibeam@163.com>
2022-01-07 11:17:31 +0800

    Math

--- math/mpl_squares.py
+++ math/mpl_squares.py
@@ -9,6 +9,6 @@ if __name__ == '__main__':
     plt.xlabel("Value", fontsize=14)
     plt.ylabel("Square of value", fontsize=14)
 
-    #设置刻度标记的大小
+    # 设置刻度标记的大小
     plt.tick_params(axis='both', labelsize=14)
     plt.show()
