9b469bb7f721c84c42191a36f29ad4ac94365ddb
unibeam <unibeam@163.com>
2022-01-07 14:09:25 +0800

    Math

--- math/die_visual.py
+++ math/die_visual.py
@@ -1,1 +1,31 @@
+import pygal
 from die import Die
+
+if __name__ == '__main__':
+    # 创建一个D6
+    die_1 = Die()
+    die_2 = Die(10)
+
+    # 投几次骰子，并将结果放在一个列表中
+    results = []
+    for roll_num in range(50000):
+        result = die_1.roll() + die_2.roll()
+        results.append(result)
+
+    frequencies = []
+    max_result = die_1.num_sides + die_2.num_sides
+    for value in range(2, max_result + 1):
+        frequency = results.count(value)
+        frequencies.append(frequency)
+
+    # 对结果进行可视化
+    hist = pygal.Bar()
+
+    hist.title = "Result of rolling two D6 dice 1000 times."
+    hist.x_labels = ['2', '3', '4', '5', '6', '7', '8', '9',
+                     '10', '11', '12', '13', '14', '15', '16']
+    hist.x_title = "Result"
+    hist.y_title = "Frequency of Result"
+
+    hist.add('D6+D10', frequencies)
+    hist.render_to_file('div_visual.svg')
