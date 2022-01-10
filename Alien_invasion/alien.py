bbc741302c769449462ea4bc330c0be92299b1f3
unibeam <beyondevery@live.com>
2022-01-10 16:34:36 +0800

    web Django

--- Alien_invasion/alien.py
+++ Alien_invasion/alien.py
@@ -13,7 +13,7 @@ class Alien(Sprite):
         self.ai_settings = ai_settings
 
         # 加载外星人图像，并设置其rect属性
-        self.image = pygame.image.load('Alien_invasion/images/alien.bmp')
+        self.image = pygame.image.load('images/alien.bmp')
         self.rect = self.image.get_rect()
 
         # 每个外星人初始都在屏幕左上角附近
